c99f7538f0a02f5f46cc6b622a6526fe32aa0ef4
dedan-wamalwa <wodedan000@gmail.com>
2022-02-19 13:18:32 +0300

    multidimensional arrays and their functions

--- numpy_1.py
+++ numpy_1.py
@@ -4,3 +4,13 @@ import numpy as np
 # an array in numpy is homogenous
 x=np.array([1,2,3,4])
 print(x[0])
+# multidimensional arrays
+x2=np.array([[1,2,3],[5,6,7],[8,9,10],[11,12,13]])
+print(x2)
+print(x2[0][2])
+# array methods
+print("methods")
+print(x2.ndim) #no of dimensions of the array
+print(x2.size) #total no. of items stored in the array
+print(x2.shape) #no of elements stored in each dimension of the array
+
