83f906f89557412dc0c5f462735733992e262599
dedan-wamalwa <wodedan000@gmail.com>
2022-03-03 20:55:26 +0300

    more learning

--- numpy_1.py
+++ numpy_1.py
@@ -9,8 +9,12 @@ x2=np.array([[1,2,3],[5,6,7],[8,9,10],[11,12,13]])
 print(x2)
 print(x2[0][2])
 # array methods
-print("methods")
+print("===methods===")
 print(x2.ndim) #no of dimensions of the array
 print(x2.size) #total no. of items stored in the array
-print(x2.shape) #no of elements stored in each dimension of the array
+print(x2.shape) #tuple showing the no. of elements stored in each dimension of the array
+print("=====")
+print(np.append(x,17)) #appending items to the array
+print(np.delete(x,1))
+print(np.arange(2,10,3))
 
