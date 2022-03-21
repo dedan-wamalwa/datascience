2162e739a523cfff7ea9e28ebf71778494e6a21f
dedan-wamalwa <wodedan000@gmail.com>
2022-03-21 22:30:40 +0300

    using reshape in numpy

--- numpy_1.py
+++ numpy_1.py
@@ -2,7 +2,7 @@
 import numpy as np
 # creating an array in numpy
 # an array in numpy is homogenous
-x=np.array([1,2,3,4])
+'''x=np.array([1,2,3,4])
 print(x[0])
 # multidimensional arrays
 x2=np.array([[1,2,3],[5,6,7],[8,9,10],[11,12,13]])
@@ -16,5 +16,11 @@ print(x2.shape) #tuple showing the no. of elements stored in each dimension of t
 print("=====")
 print(np.append(x,17)) #appending items to the array
 print(np.delete(x,1))
-print(np.arange(2))
-
+print(np.arange(2))'''
+print("====")
+#reshape- usd to change the shape of an existing array
+#the new array to be created must have the same no of  elements as the original array
+zz= np.array([20,21,22,23,24,25])
+print(zz)
+zz2= zz.reshape(3,2)
+print(zz2)
