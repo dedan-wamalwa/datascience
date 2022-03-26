07b6d815778fd690adf43e5b6967feb26fd8b4ba
dedan-wamalwa <wodedan000@gmail.com>
2022-03-26 22:59:34 +0300

    values within one standard dev from the mean

--- ds1.py
+++ ds1.py
@@ -5,13 +5,17 @@ import pandas as pd
 df=pd.read_csv("datascience/names_ages.csv")
 # print(df)
 # print(df["Age"])
-marray=pd.array(df["Age"])
+marray=np.array(df["Age"])
 mean_=np.mean(marray)
 mean_=round(mean_,2)
 median_=np.median(marray)
 stddev=np.std(marray)
 stddev=round(stddev,2)
+within_one_stddev=[]
+for x in marray:
+    if x>=(mean_-stddev) and x<=(mean_+stddev):
+        within_one_stddev.append(x)
 print("The mean of the ages is:",mean_)
 print("The median of the ages is:",median_)
 print("The standard deviation of the ages is:",stddev)
-# print("Ages within one standard deviation are:")
+print("Ages within one standard deviation are:",within_one_stddev)
